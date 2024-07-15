0bc06487f103985f398e657514ade47fcf517b4a
Hubert Bosiacki <adelajdczyk@gmail.com>
2024-07-15 20:00:11 +0100

    feat: Add custom bubbletea handler

--- main.go
+++ main.go
@@ -1,13 +1,25 @@
 package main
 
-import (
-	"errors"
-	"net"
+// An example Bubble Tea server. This will put an ssh session into alt screen
+// and continually print up to date terminal information.
 
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/log"
 	"github.com/charmbracelet/ssh"
 	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/bubbletea"
 	"github.com/charmbracelet/wish/logging"
+	"github.com/muesli/termenv"
 )
 
 const (
@@ -16,26 +28,11 @@ const (
 )
 
 func main() {
-	srv, err := wish.NewServer(
-		// The address the server will listen to.
+	s, err := wish.NewServer(
 		wish.WithAddress(net.JoinHostPort(host, port)),
-
-		// The SSH server need its own keys, this will create a keypair in the
-		// given path if it doesn't exist yet.
-		// By default, it will create an ED25519 key.
 		wish.WithHostKeyPath(".ssh/id_ed25519"),
-
-		// Middlewares do something on a ssh.Session, and then call the next
-		// middleware in the stack.
 		wish.WithMiddleware(
-			func(next ssh.Handler) ssh.Handler {
-				return func(sess ssh.Session) {
-					wish.Println(sess, "Hello, world!")
-					next(sess)
-				}
-			},
-
-			// The last item in the chain is the first to be called.
+			myCustomBubbleteaMiddleware(),
 			logging.Middleware(),
 		),
 	)
@@ -43,9 +40,90 @@ func main() {
 		log.Error("Could not start server", "error", err)
 	}
 
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
 	log.Info("Starting SSH server", "host", host, "port", port)
-	if err = srv.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
-		// We ignore ErrServerClosed because it is expected.
-		log.Error("Could not start server", "error", err)
+	go func() {
+		if err = s.ListenAndServe(); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+			log.Error("Could not start server", "error", err)
+			done <- nil
+		}
+	}()
+
+	<-done
+	log.Info("Stopping SSH server")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer func() { cancel() }()
+	if err := s.Shutdown(ctx); err != nil && !errors.Is(err, ssh.ErrServerClosed) {
+		log.Error("Could not stop server", "error", err)
 	}
 }
+
+// You can write your own custom bubbletea middleware that wraps tea.Program.
+// Make sure you set the program input and output to ssh.Session.
+func myCustomBubbleteaMiddleware() wish.Middleware {
+	newProg := func(m tea.Model, opts ...tea.ProgramOption) *tea.Program {
+		p := tea.NewProgram(m, opts...)
+		go func() {
+			for {
+				<-time.After(1 * time.Second)
+				p.Send(timeMsg(time.Now()))
+			}
+		}()
+		return p
+	}
+	teaHandler := func(s ssh.Session) *tea.Program {
+		pty, _, active := s.Pty()
+		if !active {
+			wish.Fatalln(s, "no active terminal, skipping")
+			return nil
+		}
+		m := model{
+			term:   pty.Term,
+			width:  pty.Window.Width,
+			height: pty.Window.Height,
+			time:   time.Now(),
+		}
+		return newProg(m, append(bubbletea.MakeOptions(s), tea.WithAltScreen())...)
+	}
+	return bubbletea.MiddlewareWithProgramHandler(teaHandler, termenv.ANSI256)
+}
+
+// Just a generic tea.Model to demo terminal information of ssh.
+type model struct {
+	term   string
+	width  int
+	height int
+	time   time.Time
+}
+
+type timeMsg time.Time
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case timeMsg:
+		m.time = time.Time(msg)
+	case tea.WindowSizeMsg:
+		m.height = msg.Height
+		m.width = msg.Width
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		}
+	}
+	return m, nil
+}
+
+func (m model) View() string {
+	s := "Your term is %s\n"
+	s += "Your window size is x: %d y: %d\n"
+	s += "Time: " + m.time.Format(time.RFC1123) + "\n\n"
+	s += "Press 'q' to quit\n"
+	return fmt.Sprintf(s, m.term, m.width, m.height)
+}
+
